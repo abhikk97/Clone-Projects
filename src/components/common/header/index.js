08624b2c356f42e4276999f051cd00e8a69dedd0
Abhikaushik21 <abhikaushik573@gmail.com>
2022-04-12 16:43:38 +0530

    header-complete-commit

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -14,6 +14,17 @@ const Header = () => {
                       </div>
                       <i class="fi fi-rr-caret-down absolute-center"></i>
                 </div>
+                <div className='location-search-seperator'></div>
+                <div className='header-search-bar'>
+                <i className="fi fi-rr-search absolute-center search-icon"></i>
+                <input placeholder='Search for restaurant, cuisine or a dish' className='search-input'/>
+                </div>
+            </div>
+            <div className='profile-wrapper'>
+                <img className='header-profile-image' alt='profile' src='https://b.zmtcdn.com/images/user_avatars/mug_2x.png?fit=around%7C100%3A100&crop=100%3A100%3B%2A%2C%2A'/>
+                <span className='header-username'>Abhishek</span>
+                <i class="fi fi-rr-angle-down absolute-center profile-option-icon"></i>
+
             </div>
 
         </div>
