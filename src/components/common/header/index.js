a221f7cd81e0397576421b5ce2befc1602db9d10
Abhikaushik21 <abhikaushik573@gmail.com>
2022-04-12 13:05:14 +0530

    header-css-commit1

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -5,10 +5,14 @@ const Header = () => {
     return (<div className='max-width header'>
         <img src='https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png' alt='Zamato logo' className='header-logo'/>
 
-        <div className='header-left'>
+        <div className='header-right'>
             <div className='header-location-search-container'>
                 <div className='location-wrapper'>
-                    <div className='location-icon-name'></div>
+                    <div className='location-icon-name '>
+                    <i className="fi fi-rr-marker absolute-center location-icon"></i>
+                    <div>Banglore</div>                  
+                      </div>
+                      <i class="fi fi-rr-caret-down absolute-center"></i>
                 </div>
             </div>
 
